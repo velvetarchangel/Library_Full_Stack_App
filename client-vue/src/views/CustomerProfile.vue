3abb7f389e789255e8ad74521da2722a2ca8d698
velvetarchangel <h.dastidar@gmail.com>
2022-03-23 18:22:57 -0600

    Adding more data to tables, adding front end skeleton

---
+++ client-vue/src/views/CustomerProfile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>User profile for user {{ this.customer_card }}</div>
+    <div>Librarian profiles is {{ this.admin_card }}</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      customer_card: this.$route.params.customer_card,
+      admin_card: this.$route.params.admin_card,
+    };
+  },
+};
+</script>
