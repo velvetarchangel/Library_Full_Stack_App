b0f53aedad92f5319439a9c5892a30cf02512bbf
velvetarchangel <h.dastidar@gmail.com>
2022-03-24 01:13:25 -0600

    Added front end components for librarian view

--- client-vue/src/views/LibrarianProfile.vue
+++ client-vue/src/views/LibrarianProfile.vue
@@ -1,12 +1,296 @@
 <template>
-  <div>User profile for the librarian is {{ this.card_no }}</div>
+  <div class="container" width="80%">
+    <event-modal ref="modal"></event-modal>
+    <v-toolbar color="orange accent-1">
+      <v-app-bar-nav-icon class="hidden-sm-and-down"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-h6 mr-6 hidden-sm-and-down">
+        {{ name }}
+      </v-toolbar-title>
+      <v-autocomplete
+        v-model="model"
+        :items="items"
+        :loading="isLoading"
+        :search-input.sync="search"
+        chips
+        clearable
+        hide-details
+        hide-selected
+        item-text="name"
+        item-value="symbol"
+        label="Search..."
+        solo
+      >
+        <template v-slot:no-data>
+          <v-list-item>
+            <v-list-item-title> Search </v-list-item-title>
+          </v-list-item>
+        </template>
+        <template v-slot:selection="{ attr, on, item, selected }">
+          <v-chip
+            v-bind="attr"
+            :input-value="selected"
+            color="blue-grey"
+            class="white--text"
+            v-on="on"
+          >
+            <v-icon left> mdi-bitcoin </v-icon>
+            <span v-text="item.name"></span>
+          </v-chip>
+        </template>
+        <template v-slot:item="{ item }">
+          <v-list-item-avatar
+            color="indigo"
+            class="text-h5 font-weight-light white--text"
+          >
+            {{ item.name.charAt(0) }}
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.name"></v-list-item-title>
+            <v-list-item-subtitle v-text="item.symbol"></v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-icon>mdi-bitcoin</v-icon>
+          </v-list-item-action>
+        </template>
+      </v-autocomplete>
+      <template v-slot:extension>
+        <v-tabs
+          v-model="tab"
+          :hide-slider="!model"
+          color="blue-grey"
+          slider-color="blue-grey"
+        >
+          <v-tab @click="viewCustomerList"> Customers </v-tab>
+
+          <v-tab @click="viewEventList"> Events</v-tab>
+          <v-btn class="ma-2" color="secondary" @click="signOut">
+            Sign Out
+          </v-btn>
+          <v-btn class="ma-2" color="secondary" @click="addEvent">
+            Add Event
+          </v-btn>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+    <v-container fluid fill-width>
+      <v-card width="100vw">
+        <v-data-table
+          class="elevation-1 100%"
+          v-if="showCustTab"
+          :headers="customerHeaders"
+          :items="customers"
+          :sort-by="['card_no', 'email']"
+          :sort-desc="[false, true]"
+          multi-sort
+        ></v-data-table>
+      </v-card>
+      <v-card width="100vw">
+        <v-data-table
+          class="elevation-1 100%"
+          v-if="showEventTab"
+          :headers="eventHeaders"
+          :items="events"
+          :sort-by="['card_no', 'email']"
+          :sort-desc="[false, true]"
+          multi-sort
+        ></v-data-table>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 <script>
+import EventModal from "../Components/EventModal.vue";
 export default {
+  components: { EventModal },
   data() {
     return {
+      isLoading: false,
+      items: [],
+      customerHeaders: [
+        {
+          text: "Customer name",
+          align: "start",
+          value: "name",
+        },
+        { text: "Card Number", value: "card_no" },
+        { text: "email", value: "email" },
+      ],
+      eventHeaders: [
+        {
+          text: "Event Name",
+          align: "event_name",
+          value: "event_name",
+        },
+        { text: "Event Location", value: "e_location" },
+        { text: "Event Time", value: "time" },
+        { text: "Event coordinator", value: "e_coordinator" },
+      ],
+      model: null,
+      search: null,
+      tab: null,
+      showCustTab: false,
+      showEventTab: false,
+      events: [
+        {
+          event_name: "Read together",
+          e_location: "Louise Riley Library Room 5",
+          time: "2022-03-24",
+          e_coordinator: "Michelle Obama",
+        },
+        {
+          event_name: "Read together",
+          e_location: "Louise Riley Library Room 5",
+          time: "2022-03-24",
+          e_coordinator: "Michelle Obama",
+        },
+        {
+          event_name: "Read together",
+          e_location: "Louise Riley Library Room 5",
+          time: "2022-03-24",
+          e_coordinator: "Michelle Obama",
+        },
+        {
+          event_name: "Read together",
+          e_location: "Louise Riley Library Room 5",
+          time: "2022-03-24",
+          e_coordinator: "Michelle Obama",
+        },
+        {
+          event_name: "Read together",
+          e_location: "Louise Riley Library Room 5",
+          time: "2022-03-24",
+          e_coordinator: "Michelle Obama",
+        },
+        {
+          event_name: "Read together",
+          e_location: "Louise Riley Library Room 5",
+          time: "2022-03-24",
+          e_coordinator: "Michelle Obama",
+        },
+        {
+          event_name: "Read together",
+          e_location: "Louise Riley Library Room 5",
+          time: "2022-03-24",
+          e_coordinator: "Michelle Obama",
+        },
+        {
+          event_name: "Read together",
+          e_location: "Louise Riley Library Room 5",
+          time: "2022-03-24",
+          e_coordinator: "Michelle Obama",
+        },
+      ],
+      customers: [
+        {
+          //CHANGE ME TO DYNAMIC DATA
+          card_no: 12234577,
+          name: "Himika Dastidar",
+          email: "abc@123.com",
+        },
+        {
+          card_no: 1231234324,
+          name: "Kelly Osena",
+          email: "kosena@test.com",
+        },
+        {
+          card_no: 12132131,
+          name: "Eric Tan",
+          email: "etan@test.com",
+        },
+        {
+          card_no: 12234577,
+          name: "Himika Dastidar",
+          email: "abc@123.com",
+        },
+        {
+          card_no: 1231234324,
+          name: "Kelly Osena",
+          email: "kosena@test.com",
+        },
+        {
+          card_no: 12132131,
+          name: "Eric Tan",
+          email: "etan@test.com",
+        },
+        {
+          card_no: 12234577,
+          name: "Himika Dastidar",
+          email: "abc@123.com",
+        },
+        {
+          card_no: 1231234324,
+          name: "Kelly Osena",
+          email: "kosena@test.com",
+        },
+        {
+          card_no: 12132131,
+          name: "Eric Tan",
+          email: "etan@test.com",
+        },
+        {
+          card_no: 12234577,
+          name: "Himika Dastidar",
+          email: "abc@123.com",
+        },
+        {
+          card_no: 1231234324,
+          name: "Kelly Osena",
+          email: "kosena@test.com",
+        },
+        {
+          card_no: 12132131,
+          name: "Eric Tan",
+          email: "etan@test.com",
+        },
+      ],
+      val: "",
       card_no: this.$route.params.card_no,
+      name: "Himika", // need to make this dynamic
     };
+  },
+
+  methods: {
+    viewCustomerList() {
+      this.showCustTab = true;
+      this.showEventTab = false;
+    },
+    viewEventList() {
+      this.showEventTab = true;
+      this.showCustTab = false;
+    },
+    signOut() {},
+    addEvent() {
+      this.$refs.modal.show();
+    },
+  },
+  watch: {
+    model(val) {
+      if (val != null) this.tab = 0;
+      else this.tab = null;
+    },
+    search() {
+      // Items have already been loaded
+      if (this.items.length > 0) return;
+      this.isLoading = true;
+
+      // To be fixed to incorporate our own data
+      fetch("https://api.coingecko.com/api/v3/coins/list")
+        .then((res) => res.clone().json())
+        .then((res) => {
+          this.items = res;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => (this.isLoading = false));
+    },
   },
 };
 </script>
+<style scoped>
+.container {
+  /* position: absolute; */
+  height: 80%;
+  top: 5%;
+}
+</style>
